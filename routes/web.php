f61038a743259f84312ef5bd495ce63c6c00fa3b
Niloy PC <aymanniloy14567@gmail.com>
2024-05-21 01:53:46 +0600

    homepage

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\HomepageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomepageController::class, 'homepage'])->name('homepage');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
