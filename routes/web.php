daa2fb0f42bfa0bd379c18f71b7a308a3efafc0c
Niloy PC <aymanniloy14567@gmail.com>
2024-05-21 19:07:42 +0600

    backend dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\DashboardController;
 use App\Http\Controllers\Frontend\HomepageController;
+
 
 Route::get('/', [HomepageController::class, 'homepage'])->name('homepage');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
